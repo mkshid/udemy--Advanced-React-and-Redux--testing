d9ddedeccd9d9b26b98e9685e33f552ece8af651
M. Hassan Khurshid <mkshid@gmail.com>
2017-07-02 19:11:45 +0200

    Add test and component for comment list

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CommentBox from './comment_box';
+import CommentList from './comment_list';
 
 
 export default class App extends Component {
@@ -7,6 +8,7 @@ export default class App extends Component {
     return (
         <div>
             <CommentBox />
+            <CommentList />
         </div>
     );
   }
---
+++ test/components/comment_list_test.js
@@ -0,0 +1,23 @@
+import { renderComponent, expect } from '../test_helper';
+import CommentList from '../../src/components/comment_list';
+
+
+describe('CommentList', () => {
+
+    let component;
+
+    beforeEach(()=>{
+        const props = {comments: ['new comment', 'other new comment']};
+        component = renderComponent(CommentList, null, props);
+    });
+
+    it('shows an LI for each comment', () => {
+        expect(component.find('li').length).to.equal(2);
+    });
+
+    it('shows each that is provided', () => {
+        expect(component).to.contain('new comment');
+        expect(component).to.contain('other new comment');
+     });
+    
+})
